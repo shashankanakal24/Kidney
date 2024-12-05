cb6f74564bac9609ddfe3f61c55e9b441623251a
shashankanakal24 <shashankanakal2004@gmail.com>
2024-12-05 23:41:56 +0530

    Initial commit

---
+++ src/cnnClassifier/components/model_training.py
@@ -0,0 +1,100 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+import time
+from pathlib import Path
+from cnnClassifier.entity.config_entity import TrainingConfig
+
+
+class Training:
+    def __init__(self, config: TrainingConfig):
+        self.config = config
+
+    
+    def get_base_model(self):
+        self.model = tf.keras.models.load_model(
+            self.config.updated_base_model_path
+        )
+         # Modify the model for 4 classes
+        if self.model.output_shape[-1] != 4:
+            x = self.model.layers[-2].output  # Take output from the second-last layer
+            output = tf.keras.layers.Dense(4, activation='softmax')(x)  # Add 4-class output layer
+            self.model = tf.keras.Model(inputs=self.model.input, outputs=output)
+
+        # Compile the model for training
+            self.model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),  # Adjust learning rate if needed
+            loss="categorical_crossentropy",  # Use this loss for multi-class classification
+            metrics=["accuracy"]
+        )
+
+    def train_valid_generator(self):
+
+        datagenerator_kwargs = dict(
+            rescale = 1./255,
+            validation_split=0.20
+        )
+
+        dataflow_kwargs = dict(
+            target_size=self.config.params_image_size[:-1],
+            batch_size=self.config.params_batch_size,
+            interpolation="bilinear"
+        )
+
+        valid_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+            **datagenerator_kwargs
+        )
+
+        self.valid_generator = valid_datagenerator.flow_from_directory(
+            directory=self.config.training_data,
+            subset="validation",
+            shuffle=False,
+            **dataflow_kwargs
+        )
+
+        if self.config.params_is_augmentation:
+            train_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+                rotation_range=40,
+                horizontal_flip=True,
+                width_shift_range=0.2,
+                height_shift_range=0.2,
+                shear_range=0.2,
+                zoom_range=0.2,
+                **datagenerator_kwargs
+            )
+        else:
+            train_datagenerator = valid_datagenerator
+
+        self.train_generator = train_datagenerator.flow_from_directory(
+            directory=self.config.training_data,
+            subset="training",
+            shuffle=True,
+            **dataflow_kwargs
+        )
+
+    
+    @staticmethod
+    def save_model(path: Path, model: tf.keras.Model):
+        model.save(path)
+
+
+
+    
+    def train(self):
+        self.steps_per_epoch = self.train_generator.samples // self.train_generator.batch_size
+        self.validation_steps = self.valid_generator.samples // self.valid_generator.batch_size
+
+        self.model.fit(
+            self.train_generator,
+            epochs=self.config.params_epochs,
+            steps_per_epoch=self.steps_per_epoch,
+            validation_steps=self.validation_steps,
+            validation_data=self.valid_generator
+        )
+
+        self.save_model(
+            path=self.config.trained_model_path,
+            model=self.model
+        )
+
