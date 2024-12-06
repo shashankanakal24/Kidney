9fae8a7509aae1d4eb640c38ed2ed4a477bc3923
shashankanakal24 <shashankanakal2004@gmail.com>
2024-12-07 00:44:40 +0530

    template added

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -1,30 +1,39 @@
-# import numpy as np
-# from tensorflow.keras.models import load_model
-# from tensorflow.keras.preprocessing import image
-# import os
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
 
 
 
-# class PredictionPipeline:
-#     def __init__(self,filename):
-#         self.filename =filename
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
 
 
     
-#     def predict(self):
-#         # load model
-#         model = load_model(os.path.join("model", "model.h5"))
+    def predict(self):
+        # load model
+        model = load_model(os.path.join("model", "model.h5"))
 
-#         imagename = self.filename
-#         test_image = image.load_img(imagename, target_size = (224,224))
-#         test_image = image.img_to_array(test_image)
-#         test_image = np.expand_dims(test_image, axis = 0)
-#         result = np.argmax(model.predict(test_image), axis=1)
-#         print(result)
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+        
+        class_labels = ['Cyst', 'Normal', 'Stone', 'Tumor']
 
-#         if result[0] == 1:
-#             prediction = 'Tumor'
-#             return [{ "image" : prediction}]
-#         else:
-#             prediction = 'Normal'
-#             return [{ "image" : prediction}]
+        # Map the result to the corresponding label
+        prediction = class_labels[result[0]]
+
+        # Print and return the prediction
+        print(f"Predicted Class: {prediction}")
+        return [{"image": prediction}]
+
+        # if result[0] == 1:
+        #     prediction = 'Tumor'
+        #     return [{ "image" : prediction}]
+        # else:
+        #     prediction = 'Normal'
+        #     return [{ "image" : prediction}]
